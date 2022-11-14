dcf043f7fe9c9e4b160c26fcb51292a85f170633
IsaacThaJunior <isaacjunez669@gmail.com>
2022-11-14 20:19:47 +0100

    created the navbar and header

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import navbar from './components/navbar.vue'
+import HeaderComp from './components/HeaderComp.vue';
+</script>
+
+<template>
+  <div>
+    <navbar />
+    <HeaderComp />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
