a06ec066724c20394e68b8e0c07d1c690e36d078
IsaacThaJunior <isaacjunez669@gmail.com>
2022-11-16 20:57:47 +0100

    finished the media queries

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -2,18 +2,18 @@
   <div>
     <div class="bg-white md:h-85 flex justify-center items-center flex-col px-6 pt-4 py-8 pb-12">
                 <h1
-                  class="heading-secondary font-bold inline-block text-2xl md:text-5xl lg:text-6xl uppercase transition-all pb-4 md:-mt-18"
+                  class="heading-secondary font-bold inline-block text-2xl md:text-5xl lg:text-6xl uppercase transition-all pb-4 md:pb-8 lg:pb-10"
                 >
                   Data to enrich your online business
                 </h1>
-                <p class="md:mt-6 text-md lg:text-xl leading-8 text-gray-600 sm:text-center pb-8 md:w-2/3 lg:w-1/2">
+                <p class="text-md lg:text-xl leading-8 text-gray-600 sm:text-center pb-8 md:w-2/3 lg:w-1/2">
                   Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
                   qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
                   occaecat fugiat aliqua. Lorem ipsum dolor sit amet consectetur
                   adipisicing elit.
                 </p>
 
-                <p class="md:mt-8 text:md lg:text-xl leading-8 text-gray-600 sm:text-center md:w-2/3 lg:w-1/2 pb-8">
+                <p class="text:md lg:text-xl leading-8 text-gray-600 sm:text-center md:w-2/3 lg:w-1/2 pb-8 md:pb-8 lg:pb-12">
                   Ratione perspiciatis delectus, recusandae at dignissimos
                   doloribus eius eaque ipsum nesciunt voluptas quis in pariatur
                   libero eum. Mollitia quae molestias reiciendis dicta, autem et
@@ -21,10 +21,10 @@
                   deserunt harum est! Explicabo omnis labore at et impedit
                   maiores!
                 </p>
-                <div class="md:mt-20 flex gap-x-4 sm:justify-center flex-col md:flex-row">
+                <div class="flex gap-x-4 justify-center flex-col md:flex-row md:w-1/3 md:text-center">
                   <a
                     href="#"
-                    class="inline-block w-3/4 text-center mx-auto mb-4 rounded-lg bg-green-600 px-4 py-2.5 text-xl font-semibold leading-7 text-white shadow-sm ring-1 ring-green-600 hover:-translate-y-2 hover:ring-green-700"
+                    class="inline-block w-1/2 text-center my-auto mx-auto mb-4 rounded-lg bg-green-600 px-4 py-2.5 text-xl font-semibold leading-7 text-white shadow-sm ring-1 ring-green-600 hover:-translate-y-2 hover:ring-green-700 md:mb-0"
                   >
                     Explore now
                     <span class="text-white" aria-hidden="true"
@@ -33,7 +33,7 @@
                   </a>
                   <a
                     href="#"
-                    class="inline-block rounded-lg px-4 py-2.5 text-lg md:text-xl font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                    class="inline-block rounded-lg px-4 py-2.5 text-lg font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20 w-3/4"
                   >
                     Check out our Sessions
                     <span class="text-gray-600" aria-hidden="true">&rarr;</span>
--- src/components/about.vue
+++ src/components/about.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex justify-center bg-white/90 pb-16">
-    <section class="pb-24 px-4 md:px-12 pt-20">
+  <div class="flex justify-center bg-white/90 pb-16 md:pb-0">
+    <section class="pb-24 px-4 md:px-8 pt-20">
       <div class="text-center mb-14 md:mb-28">
         <h2 class="heading-secondary font-bold inline-block text-4xl md:text-5xl uppercase transition-all">
           Explore the world with us!
@@ -45,13 +45,13 @@
             <img
               src="../assets/victor2.jpg"
               alt="Photo 2"
-              class="w-5/12 shadow-lg absolute md:z-10 transition-all duration-300 outline-offset-8  hover:scale-110  hover:-translate-y-2 hover:shadow-md hover:z-20 md:right-28 md:top-0 left-1/4 z-30"
+              class="w-5/12 shadow-lg absolute md:z-10 transition-all duration-300 outline-offset-8  hover:scale-110  hover:-translate-y-2 hover:shadow-md z-30 hover:z-20 md:right-28 md:top-8 left-1/4"
             />
 
             <img
               src="../assets/victor3.jpg"
               alt="Photo 3"
-              class="w-5/12 shadow-lg absolute z-10 transition-all duration-300 outline-offset-8  hover:scale-110  hover:-translate-y-2 hover:shadow-md hover:z-20 md:left-1/4 md:top-20 right-0"
+              class="w-5/12 shadow-lg absolute z-10 transition-all duration-300 outline-offset-8  hover:scale-110  hover:-translate-y-2 hover:shadow-md hover:z-20 md:top-20 right-14"
             />
           </div>
         </div>
@@ -61,42 +61,6 @@
 </template>
 
 <style scoped>
-
-@media screen and (max-width: 56.25em) {
-  .composition__photo {
-    width: 33.3333%;
-    box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.4);
-    position: relative;
-    float: left;
-  }
-
-  .composition__photo--p1 {
-    top: 0;
-    transform: scale(1.2);
-  }
-
-  .composition__photo--p2 {
-    top: -1rem;
-    transform: scale(1.3);
-    z-index: 100;
-  }
-
-  .composition__photo--p3 {
-    top: 0;
-    left: 0;
-    transform: scale(1.1);
-  }
-}
-
-@media screen and (max-width: 37.5em) {
-  .composition__photo {
-    width: 50%;
-    box-shadow: 0 1.5rem 4rem rgba(0, 0, 0, 0.4);
-    position: relative;
-    float: left;
-  }
-
-}
 
 .heading-secondary {
   background-image: linear-gradient(to right, #7ed56f, #28b485);
